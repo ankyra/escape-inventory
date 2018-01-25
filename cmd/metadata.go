bb9a2c168139854347101372a931d086f1c67957
Bart Spaans <bart.spaans@gmail.com>
2018-01-25 15:20:06 +0000

    Update vendored escape-core

--- cmd/metadata.go
+++ cmd/metadata.go
@@ -1,3 +1,3 @@
 package cmd
 
-const InventoryVersion = "0.13.19"
+const InventoryVersion = "0.14.1"
--- vendor/github.com/ankyra/escape-core/core.go
+++ vendor/github.com/ankyra/escape-core/core.go
@@ -1,4 +1,4 @@
 package core
 
 const CurrentApiVersion = 10
-const CoreVersion = "0.10.12"
+const CoreVersion = "0.10.13"
